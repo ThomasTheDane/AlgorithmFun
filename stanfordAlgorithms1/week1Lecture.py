26b044b67ab6bbf7f69b7b749b1b35eadf90144a
Nattestad, Thomas <Thomas_Nattestad@intuit.com>
2014-06-22 11:41:39 -0700

    failed attempt to make matrix mult equation

--- stanfordAlgorithms1/week1Lecture.py
+++ stanfordAlgorithms1/week1Lecture.py
@@ -1,32 +1,39 @@
-def countInversions(start, count):
-	print "top ", count
-	if(len(start) == 1):
-		return start, count
-	X, c1 = countInversions(start[:len(start)/2], count)
-	Y, c2 = countInversions(start[len(start)/2 :], count)
-	return countSplitInversions(X, Y, c1+c2)
+def multiplyMatrix(X, Y):
+	print X, Y
+	if(len(X) == 1):
+		print 'root root bitches'
+		return X * Y
+	if(len(X) == 2):
+		print 'almost root'
+		print X, Y
+		return X[0][0] * Y[0][0]
 
-def countSplitInversions(A, B, count):
-	C = list()
-	n = len(A) + len(B)
-	i, j = 0, 0
-	for k in range(0, n):
-		if((A[i] if i < len(A) else float('inf')) < (B[j] if j < len(B) else float('inf'))):
-			C.append(A[i])
-			i+=1
-		else:
-			count += len(A) - i
-			C.append(B[j])
-			j+=1
-	print count
-	return C, count
-
-def multiplyMatrix(A, B):
+	X1 = [X[:len(X)/2][0][:len(X)/2], X[:len(X)/2][1][:len(X)/2]]
+	X2 = [X[:len(X)/2][0][len(X)/2:], X[:len(X)/2][1][len(X)/2:]]
+	X3 = [X[len(X)/2:][0][:len(X)/2], X[len(X)/2:][1][:len(X)/2]]
+	X4 = [X[len(X)/2:][0][len(X)/2:], X[len(X)/2:][1][len(X)/2:]]
 	
+	Y1 = [Y[:len(Y)/2][0][:len(Y)/2], Y[:len(Y)/2][1][:len(Y)/2]]
+	Y2 = [Y[:len(Y)/2][0][len(Y)/2:], Y[:len(Y)/2][1][len(Y)/2:]]
+	Y3 = [Y[len(Y)/2:][0][:len(Y)/2], Y[len(Y)/2:][1][:len(Y)/2]]
+	Y4 = [Y[len(Y)/2:][0][len(Y)/2:], Y[len(Y)/2:][1][len(Y)/2:]]
+
+	return [[multiplyMatrix(X1, Y1)]]
+
+	# return multiplyMatrixPiece(x, y)
+
+def multiplyMatrixPiece(X, Y):
+	print 'hi'
+
+
 
 def main():
 	count = 0
-	print countInversions([8,7,6,5,4,3,2,1], count) # 5 inversions
+	# print countInversions([8,7,6,5,4,3,2,1], count) # 5 inversions
+	print multiplyMatrix([[1,2,3,4],
+						  [5,6,7,8],
+						  [9,10,11,12],
+						  [13,14,15,16]], [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 
 if __name__ == '__main__':
 	main()
